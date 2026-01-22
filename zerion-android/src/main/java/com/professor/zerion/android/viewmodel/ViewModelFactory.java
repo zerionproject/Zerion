6bacb3b07463d19479f57aacd96e697638fcbaf6
Zerion Project <noreply@zerion.local>
2026-01-22 16:42:49 +0100

    Suppress unchecked cast warning in ViewModelFactory

--- zerion-android/src/main/java/com/professor/zerion/android/viewmodel/ViewModelFactory.java
+++ zerion-android/src/main/java/com/professor/zerion/android/viewmodel/ViewModelFactory.java
@@ -24,6 +24,7 @@ class ViewModelFactory implements ViewModelProvider.Factory {
 		this.creators = creators;
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public <T extends ViewModel> T create(Class<T> modelClass) {
 		Provider<? extends ViewModel> creator = creators.get(modelClass);
