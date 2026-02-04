a1fc4f1472ece4abd5b1b12810a62a5cd3f4df41
Zerion Project <noreply@zerion.local>
2026-02-04 13:22:51 +0100

    Fix MessageTypes visibility: make public and add Sender Keys constants

--- briar-core/src/main/java/org/briarproject/briar/messaging/MessageTypes.java
+++ briar-core/src/main/java/org/briarproject/briar/messaging/MessageTypes.java
@@ -1,10 +1,12 @@
 package org.briarproject.briar.messaging;
 
-interface MessageTypes {
+public interface MessageTypes {
 
 	int PRIVATE_MESSAGE = 0;
 	int ATTACHMENT = 1;
 	int VOICE_SIGNAL = 2;
 	int ATTACHMENT_MANIFEST = 3;
 	int ATTACHMENT_CHUNK = 4;
+	int SENDER_KEY_DISTRIBUTION = 5;
+	int REKEY_REQUEST = 6;
 }
